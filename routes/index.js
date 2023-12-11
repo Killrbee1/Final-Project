0c359b6dcb25bd759461934ee8cbf808c09bee39
Killrbee1 <gmuninger14@gmail.com>
2023-12-10 21:26:21 -0600

    Reset

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Score Tracker' });
-});
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Score Tracker' });
+  });
+
 
 module.exports = router;
